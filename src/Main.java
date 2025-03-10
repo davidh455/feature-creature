4689a269557752cdb02d654baedc91951fb8dcf6
davidh455 <davidhoang455@gmail.com>
2025-03-10 13:41:03 -0700

    Creature-Feautre add, hero monster creature

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        Creature creature = new Creature("Dragon", 40, 10, 200);
+        Hero hero = new Hero("Hero", 20, 5, 0, "Conan", "Barbarian");
+        Monster monster = new Monster("Monster", 40, 10, 500, "Chaotic");
+        System.out.println(creature);
+        System.out.println(hero);
+        System.out.println(monster);
+    }
 }
